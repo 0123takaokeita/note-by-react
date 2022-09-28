62da4dca980992150a0fc6ff4d7e6015f2058a4c
Takao <k.takao@neogenia.co.jp>
2022-09-29 02:21:22 +0900

    feat: main 側 css 追加

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,19 @@
 import './Main.css'
 
 const Main = (): JSX.Element => {
-  return <div>Main</div>
+  return(
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea  placeholder='ノート内容を記入'></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">タイトル</h1>
+        <div className="markdown-preview">ノート内容</div>
+      </div>
+    </div>
+
+  )
 }
 
 export default Main;
